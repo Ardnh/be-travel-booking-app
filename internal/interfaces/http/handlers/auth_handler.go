b401d48861dd184debe60262cdc40c4827d1132a
Muhammad Ardan Hilal <muhammadardanhilal@MacBook-Air-Muhammad-2.local>
2026-05-26 17:54:46 +0700

    update auth and vendor

--- internal/interfaces/http/handlers/auth_handler.go
+++ internal/interfaces/http/handlers/auth_handler.go
@@ -39,8 +39,6 @@ func (h *AuthHandler) Login(c fiber.Ctx) error {
 		return httpResponses.HandleError(c, err)
 	}
 
-	// set cookie setelah login berhasil
-
 	return httpResponses.NewSuccessResponse(c, fiber.StatusOK, "Login successful", result)
 }
 
